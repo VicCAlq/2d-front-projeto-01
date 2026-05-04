c2983faa6a24f5571d1fc6d88163e67c5f3c6255
livia-barros-nave <livia.barros@nave.org.br>
2026-05-04 15:16:24 -0300

    rodape feito por David Luduvico

---
+++ src/components/rodape.jsx
@@ -0,0 +1,9 @@
+export default function Rodape() {
+  return (
+    <footer>
+      <a href="https://github.com/VicCAlq/2d-front-projeto-01/tree/4-Jennifer-EduardoCristiano-Livia-Petrus-DavidLuduvico">GitHub</a>
+      
+      <p>Criado por: Livia, Jennifer, Petrus, Eduardo, David</p>
+    </footer>
+  );
+}
