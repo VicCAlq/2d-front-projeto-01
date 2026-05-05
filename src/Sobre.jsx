8ee9ce9e8f76027bcacc3dd6e2b22e26ad8b8070
caua-rafael-nave <caua.rafael@nave.org.br>
2026-05-05 14:47:36 -0300

    atvdd

---
+++ src/Sobre.jsx
@@ -0,0 +1,19 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre o Projeto</h1>
+
+      <p><strong>Gustavo:</strong> ficou com a parte do cabeçalho, organização das páginas e navegação do site. Também integrou todo o projeto.</p>
+
+      <p><strong>Gabriel Leal:</strong> fez o conteúdo, principalmente da página “Sobre”, organizando as informações.</p>
+
+      <p><strong>Breno:</strong> cuidou do rodapé com GitHub e créditos.</p>
+
+      <p><strong>Brendha:</strong> fez o menu e o botão com nome, foto e clique.</p>
+
+      <p><strong>Rafael:</strong> fez os cards com os 4 códigos dos alunos.</p>
+    </div>
+  );
+}
+
+export default Sobre;
